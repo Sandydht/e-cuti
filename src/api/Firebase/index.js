3d7172a189df5dcfe0a30caaaea2c234dc215359
Sandydht <sandydwi75@gmail.com>
2020-08-28 03:49:18 +0700

    Update

--- src/api/Firebase/index.js
+++ src/api/Firebase/index.js
@@ -4,14 +4,13 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBlrpmeUV2DckBfbZiEKqGzy43OlQY-vc",
-  authDomain: "e-cuti-53b10.firebaseapp.com",
-  databaseURL: "https://e-cuti-53b10.firebaseio.com",
-  projectId: "e-cuti-53b10",
-  storageBucket: "e-cuti-53b10.appspot.com",
-  messagingSenderId: "830315545153",
-  appId: "1:830315545153:web:b2fdb92b097dd8d81f3cc0",
-  measurementId: "G-FEPZ48V8RK"
+  apiKey: "AIzaSyC4M34jNb7-ELfr5fZKH0xJ9A1ofLHJEhY",
+  authDomain: "e-cuti-5a43b.firebaseapp.com",
+  databaseURL: "https://e-cuti-5a43b.firebaseio.com",
+  projectId: "e-cuti-5a43b",
+  storageBucket: "e-cuti-5a43b.appspot.com",
+  messagingSenderId: "874192537964",
+  appId: "1:874192537964:web:6949299f1e6676aedc4b49",
+  measurementId: "G-YFH5P5QN8X"
 };
-
 export default firebase.initializeApp(firebaseConfig);
