d62d5beff18cbd0e5bd90eadd5ca2dbe329ed38f
Sandydht <sandydwi75@gmail.com>
2020-09-11 15:15:54 +0700

    Update notifikasi

--- src/molecules/NotifikasiPengajuan.jsx
+++ src/molecules/NotifikasiPengajuan.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import Axios from "axios";
 
 // Material UI
 import Badge from '@material-ui/core/Badge';
@@ -6,16 +7,24 @@ import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Divider from '@material-ui/core/Divider';
 
 // Icons
 import NotificationsIcon from '@material-ui/icons/Notifications';
+
+// React router dom
+import { NavLink } from "react-router-dom";
 
 class NotifikasiPengajuan extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      anchorEl: null
+      anchorEl: null,
+      data: [],
+      isLoading: true
     };
+
+    this.__subscribe = false;
   }
 
   handleOpen = (event) => {
@@ -30,8 +39,34 @@ class NotifikasiPengajuan extends Component {
     });
   };
 
+  notifikasiPengajuan = (data) => {
+    this.setState({
+      isLoading: false,
+      data
+    });
+  };
+
+  componentDidMount() {
+    this.__subscribe = true;
+    Axios.get("/getNotifikasiPengajuan")
+      .then((res) => {
+        if (this.__subscribe) {
+          this.notifikasiPengajuan(res.data);
+        }
+      })
+      .catch(() => {
+        this.setState({
+          isLoading: false
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    this.__subscribe = false;
+  }
+
   render() {
-    const { anchorEl } = this.state;
+    const { anchorEl, data } = this.state;
     const open = Boolean(anchorEl);
 
     return (
@@ -43,7 +78,7 @@ class NotifikasiPengajuan extends Component {
             color="inherit"
             onClick={this.handleOpen}
           >
-            <Badge badgeContent={1} color="secondary">
+            <Badge badgeContent={data.length} color="secondary">
               <NotificationsIcon />
             </Badge>
           </IconButton>
@@ -64,7 +99,21 @@ class NotifikasiPengajuan extends Component {
             horizontal: 'center',
           }}
         >
-          <MenuItem onClick={this.handleClose}>Tidak ada notifikasi</MenuItem>
+          {
+            data.map((value, index) => (
+              <Fragment>
+                <MenuItem
+                  onClick={this.handleClose}
+                  key={index}
+                  component={NavLink}
+                  to={`/beranda/${value.cutiId}`}
+                >
+                  {value.cutiId}
+                </MenuItem>
+                <Divider />
+              </Fragment>
+            ))
+          }
         </Menu>
       </Fragment>
     );
